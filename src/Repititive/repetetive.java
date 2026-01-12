7fe0f89922f2e35896eb4f450b1bc3e4fe23b674
Yatiraj-upadhyay <yatiraj.kumar@gmail.com>
2026-01-12 15:34:06 +0530

    Repetetive completed

--- src/Repititive/repetetive.java
+++ src/Repititive/repetetive.java
@@ -9,8 +9,19 @@ public class repetetive {
         int a=1;
         int m=0;
         int n=str.length();
-        for(char ch:str.toCharArray()){
+        for(int i=1;i<n;i++){
+            if(str.charAt(i)==str.charAt(i-1)){
+                a++;
+            }
+            else{
+                if(a>m){
+                    m=a;
+                }
+                a=1;
+            }
 
         }
+        m=Math.max(m,a);
+        System.out.println(m);
     }
 }
